c35ff3954e15f60b5196ced39867590fe79db5d7
Michael <mjfalahee@gmail.com>
2022-01-21 01:36:35 -0800

    got it working, but need to change the code so it's not always updating the DOM

--- overviewDOM.js
+++ overviewDOM.js
@@ -4,42 +4,69 @@ export async function main(ns) {
     //first add scripts running on home
     //other ideas come later
     function addTableRow(heading, rows) {
+        var count = 0
         var HTMLEles = document.getElementsByTagName('tbody')
         var tableBody = HTMLEles[0]
-        var currentTB = tableBody
-        //clear existing nodes?
+        var hR; var hH; var hP;
+        // clear existing nodes
         var existing = tableBody.querySelectorAll('#tagged')
-        // console.log(existing)
-        // existing.forEach(ele => {
-        //     ele.remove()
-        // })
-
-        //         var titleHead = document.createElement('th')
-        //         var headingP = document.createElement('p')
-        //         titleHead.setAttribute('class', "MuiTableRow-root css-u43k8v")
-        //         titleHead.setAttribute("id", "tagged")
-                
-        //         headingP.setAttribute("id", "tagged")
-        //         headingP.setAttribute('class', "MuiTypography-root MuiTypography-body1 jss11 css-1bubjwk")
-        // headingP.textContent = heading
-        // titleHead.appendChild(headingP);
-        // currentTB.appendChild(titleHead)
-        // rows.forEach((element) => {
-        //             var row = document.createElement('tr')
-        //             var rowP = document.createElement('p')
-        //             row.setAttribute("class", "jss10 MuiTableCell-root MuiTableCell-body MuiTableCell-sizeMedium css-hadb7u")
-        //             row.setAttribute("id", "tagged")
-        //             rowP.setAttribute("class", "MuiTypography-root MuiTypography-body1 jss12 css-f38fma")
-        //             rowP.setAttribute("id", "tagged")
-        //             rowP.textContent = element
-        //             row.appendChild(rowP)
-        //             currentTB.appendChild(row)
-        // })
+        existing.length > 0 ? existing.forEach((e) => e.remove()) : null;
+        var info = []
+        // heading first
+        if (heading) {
+            hR = document.createElement('tr')
+            hH = document.createElement('th')
+            hP = document.createElement('p')
+    
+            // hR.setAttribute("class","MuiTableRow-root MuiTableCell-body MuiTableCell-sizeMedium css-hadb7u")
+            hR.setAttribute("id", "tagged")
+            hR.setAttribute("display",'inherit')
+            // hH.setAttribute('class', "MuiTableRow-root css-u43k8v")
+            hH.setAttribute("id", "tagged")
+            hP.setAttribute('class', "MuiTypography-root MuiTypography-body1 jss11 css-1bubjwk")
+            hP.setAttribute("id", "tagged")
+            hP.textContent = heading
+            //append!
+            tableBody.appendChild(hR)
+            hR.appendChild(hH)
+            hH.appendChild(hP)
+        }
+        // 
+        if (tableBody.children.length > 0 && rows && rows.length > 0) {
+            rows.forEach(script => {
+                var rR; var rP; var rH
+                rR = document.createElement('tr')
+                rH = document.createElement('th')
+                rP = document.createElement('p')
+                // rR.setAttribute("class", "jss10 MuiTableRow-root MuiTableCell-body MuiTableCell-sizeMedium css-hadb7u")
+                rH.setAttribute('class', "MuiTableRow-root css-u43k8v")
+                rH.setAttribute('scope', "row")
+                rH.setAttribute('colspan', 2)
+                rR.setAttribute("id", "tagged")
+                rP.setAttribute("class", "MuiTypography-root MuiTypography-body1 jss12 css-f38fma")
+                rH.setAttribute("id", "tagged")
+                rP.setAttribute("id", `tagged-${count}`)
+                rP.textContent = script
+                rR.appendChild(rH)
+                rH.appendChild(rP)
+                tableBody.appendChild(rR)
+                count++
+                info.push([script, count])
+            })
+        }
+        return info
     }
+
+    function updateRows(strings, info) {
+        console.log(info)
+        console.log(strings)
+    }
+
+    var count = 0;
     var scripts = ns.ps('home')
     var totalRam = ns.getServerMaxRam('home')
     var strings = []
-
+    var info
     if (scripts && scripts.length > 0) {
         scripts.forEach(s => {
             var ram; var scriptString;
@@ -49,7 +76,10 @@ export async function main(ns) {
         })
     }
 
-    if (strings && strings.length > 0) {
-        addTableRow("Scripts running: ", strings)
-    }
+    if (strings && strings.length > 0 && count === 0) {
+        info = addTableRow("Scripts running: ", strings)
+        count++
+    } else if (strings && strings.length > 0) {
+        updateRows(strings, info)
+    } 
 }
