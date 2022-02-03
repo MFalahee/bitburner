9e9c11dfdbd510e937b758cd0b34d9464659e281
Michael <mjfalahee@gmail.com>
2022-02-02 19:44:08 -0800

    trying to practice simplifying the structure of my code down

--- faction.js
+++ faction.js
@@ -1,61 +1,98 @@
 /** @param {import(".").NS } ns */
 
 export async function main(ns) {
+    //only works for factions you already have access to.
+    //function accepts two arguments when you want to activate favor limits (stop getting rep with current faction at limit)
+    //[arg[0], arg[1]] ['f', favorLimit]
+
+    /**
+     * 
+     * works as is
+     * will come back later probably to automate joining factions and automatically purchase augmentations.
+     */
+
     ns.tail();
     ns.stopAction();
     ns.disableLog('ALL');
+    var count = 0;
     var mode = ns.args[0];
+    var favorLimit;
+    if (mode == 'f') {
+    (ns.args[1] != null) ? favorLimit = ns.args[1]: favorLimit = 25;
+    } else {
+        favorLimit = 999;
+    }
     var factions = ns.getPlayer().factions;
-    //setup faction max rep needed by looking at augmentation prices
-    async function getMaxRep(mode) {
-        for (var i = 0; i < factions.length; i++) {
-            var faction = factions[i]
-            //take each faction, and use ns.getAugmentationsFromFaction(faction) to get the list of augmentations
-            //get price of each augmentation-- find highest priced augmentation for each faction. Note- exclude neuroflux governor from this list of augmentations.
-            //set faction max rep as the price of the highest priced augmentation.
-            var factionMaxRep = ns.getAugmentationsFromFaction(faction).filter(a => a != 'NeuroFlux Governor').sort(function (a, b) {
-                return ns.getAugmentationPrice(b) - ns.getAugmentationPrice(a);
-            })[0]
-            ns.print(`${faction} max rep aug: ${factionMaxRep} price: ${ns.getAugmentationPrice(factionMaxRep)}`)
-            //set faction max rep as the price of the highest priced augmentation.
-            let maxRepValue = ns.getAugmentationPrice(factionMaxRep)
-            let rep = ns.getFactionRep(faction)
-            let repGainedPerCycle = rep;
-            let totalRepGained = 0
 
-            if (rep < maxRepValue) {
-                ns.print(`${faction} needs rep.`)
-                if (!ns.isBusy()) {
-                    while (rep < maxRepValue) {
-                    ns.clearLog();
+
+    /**    
+     * 
+     * take each faction, and use ns.getAugmentationsFromFaction(faction) to get the list of augmentations
+        get price of each augmentation-- find highest priced augmentation for each faction. Note- exclude neuroflux governor from this list of augmentations.
+       set faction max rep as the price of the highest priced augmentation. 
+    */
+    async function getMaxRep(faction, mode, t) {
+        var factionMaxRepAug = ns.getAugmentationsFromFaction(faction).filter(a => a != 'NeuroFlux Governor').sort(function (a, b) {
+            return ns.getAugmentationRepReq(b) - ns.getAugmentationRepReq(a);
+        })[0]
+        // ns.print(`${faction} max rep aug: ${factionMaxRepAug} price: ${ns.getAugmentationRepReq(factionMaxRepAug)}`)
+        //set faction max rep as the price of the highest priced augmentation.
+        let training = false;
+        t != null ? training = t : null;
+        let maxRepValue = ns.getAugmentationRepReq(factionMaxRepAug);
+        let rep = ns.getFactionRep(faction);
+        let repGainedPerCycle = rep;
+        let favor;
+
+        if (rep != null && rep < maxRepValue) {
+            if (!ns.isBusy() || training == true) {
+                if (rep < maxRepValue && ns.getFactionFavorGain(faction) < favorLimit) {
+                    ns.stopAction();
                     ns.workForFaction(faction, 'Hacking Contracts', true) ? null : ns.workForFaction(faction, 'Field Work', true)
                     repGainedPerCycle = rep;
                     rep = ns.getFactionRep(faction)
                     repGainedPerCycle = (rep - repGainedPerCycle);
-                    totalRepGained += repGainedPerCycle;
-                    ns.print(`WORK CYCLE === ${faction.toUpperCase()}`)
-                    ns.print(`REP:${ns.nFormat(rep, '0,0.00')}`)
-                    ns.print(`TO MAX: ${ns.nFormat(maxRepValue - rep, '0,0.00')}`)
-                    ns.print(`GAINED: ${ns.nFormat(totalRepGained, '0,0.00')}`)
-                    let favor = ns.getFactionFavorGain(faction)
-                    ns.print(`FAVOR: ${favor}`)
-                    if (mode === 'f' && favor > 50) {
-                        ns.print('FAVOR LIMIT REACHED')
+                    favor = ns.getFactionFavorGain(faction)
+                    training = true;
+                    if (mode === 'f' && favor > favorLimit) {
                         ns.stopAction();
-                        break;
                     }
-                    await ns.sleep(30000)
-                    }
+                } else if (rep >= maxRepValue){
+                    ns.stopAction();
+                    training = false;
                 }
-                // ns.workForFaction(faction, 'Hacking Contracts', true)
             }
         }
-    }
 
-    while (true) {
-        if (factions.length > 0 && mode) {
-            await getMaxRep(mode)
+        let ans = {
+            'faction': faction,
+            'rep': rep,
+            'repGainedPerCycle': repGainedPerCycle,
+            'favor': favor,
+            'factionMaxRep': factionMaxRepAug,
+            'maxRepValue': maxRepValue,
+            't': training
         }
+
+        return ans
+    }
+    let ans = {}
+    while (true) {
+        if (factions.length > 0 && mode && count < factions.length) {
+            ans = await getMaxRep(factions[count], mode, (ans.t != null ? ans.t : null))
+            ns.print(ans)
+            if (ans != null) {
+                ns.print(`WORK CYCLE === ${ans.faction.toUpperCase()}`)
+                ns.print(`REP:${ns.nFormat(ans.rep, '0,0.00')}`)
+                ns.print(`TO MAX: ${ns.nFormat(ans.maxRepValue - ans.rep, '0,0.00')}`)
+                // ns.print(`GAINED: ${ns.nFormat(ans.totalRepGained, '0,0.00')}`)
+                ns.print(`FAVOR: ${ns.nFormat(ans.favor, '0,0.00')}`)
+            }
+            ans.t == true ? null : count++;
+            ans.t == true ? ns.print('Training') : null
+            ans.t == true ? await ns.sleep(10000) : null
+        }
+        count >= factions.length ? count = 0 : null;
         await ns.sleep(2500)
     }
 }
